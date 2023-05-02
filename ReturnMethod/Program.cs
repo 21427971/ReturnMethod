c9a468ce732c9009ca55284362890ef507180071
msimangoksi@gmail.com <msimangoksi@gmail.com>
2023-05-02 10:27:03 +0200

    Add project files.

---
+++ ReturnMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Msimango KSI
+namespace ReturnMethod
+{
+    //Method that return a value with method declaration
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DecoratePlanet("Jupiter"));
+        }
+
+        static string DecoratePlanet(string planet)
+        {
+            return $"*..*..* Welcome to {planet} *..*..*";
+        }
+    }
+}
